bc6b99b54a7648b97b8bb9d0279069fe7cb72a73
licencep <licencep@infotp16.cnam.fr>
2017-09-22 14:50:17 +0200

    add producteurs

--- tp/buff.c
+++ tp/buff.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include "buff.h"
 
 int buffInit(buff* sb){
@@ -9,31 +10,64 @@ int buffInit(buff* sb){
     sb->r = 0;
     sb->w = 0;
     
+    if((sem_init(&sb->s1, 0, SIZE)) != 0){
+        perror("sem_init empty");
+        exit(EXIT_FAILURE);
+    }
+    
+     if((sem_init(&sb->s2, 0, 0)) != 0){
+        perror("sem_init empty");
+        exit(EXIT_FAILURE);
+    }
+    
+    pthread_mutex_init(&sb->mr, NULL);
+    pthread_mutex_init(&sb->mw, NULL);
+    
     return 0;
 }
 
-int bread(buff* sb){
-    
+char bread(buff* sb){
+    /*
     if(sb->r == sb->w){
         printf("read = write : empty\n");
         return 0;
     }
+    */
     
-    printf("%c",sb->tab[sb->r]);
-    sb->r = (sb->r + 1)%SIZE;
+    char res;
+    int tmp;
+    sem_wait(&sb->s2);
     
-    return 0;
+    pthread_mutex_lock(&sb->mr);
+    tmp = sb->r;
+    sb->r = (sb->r+1)%SIZE;
+    pthread_mutex_unlock(&sb->mr);
+    
+    res = sb->tab[tmp];
+    sem_post(&sb->s1);
+    
+    return res;
 }
 
 int bwrite(buff* sb, char c){
-    
+    /*
     if(sb->w == sb->r-1){
         printf("buff full\n");
         return 0;
     }
+    */
     
-    sb->tab[sb->w] = c;
+    int tmp;
+    sem_wait(&sb->s1);
+    
+    pthread_mutex_lock(&sb->mw);
+    tmp = sb->w;
     sb->w = (sb->w+1)%SIZE;
+    pthread_mutex_unlock(&sb->mw);
+    
+    sb->tab[tmp] = c;
+    
+    sem_post(&sb->s2);
     
     return 0; 
 }  
