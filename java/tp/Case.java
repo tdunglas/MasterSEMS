d06404975d4118aa23ac6da07d85a361bd22c03e
licencep <licencep@jeuneurs07.cnam.fr>
2017-09-28 12:35:08 +0200

    done av

---
+++ java/tp/Case.java
@@ -0,0 +1,13 @@
+
+public class Case{
+
+    public String value;
+    
+    public void setValue(String val){
+        value = val;
+    }
+    
+    public String getValue(){
+        return value;
+    }
+}
