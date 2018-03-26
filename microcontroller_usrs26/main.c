97241681012179643a38740bdb89eceac1efad3d
tdunglas <dunglast@gmail.com>
2018-03-26 12:29:54 +0200

    Add files via upload

--- microcontroller_usrs26/main.c
+++ microcontroller_usrs26/main.c
@@ -1,9 +1,18 @@
 #include "stm32f3xx.h"
 #include "system_stm32f3xx.h"
 
-void init_leds(void);
-void set_leds(char num, char v);
-char get_leds(char num);
+// led functions 
+void stm32f3_init_leds(void);
+void stm32f3_set_leds(char num, char v);
+char stm32f3_get_leds(char num);
+
+// button functions
+void stm32f3_init_button();
+char stm32f3_get_button(); 
+char stm32f3_just_pressed();
+char stm32f3_just_released();
+
+static unsigned char button_state;
 
 void delay() {
   for (float x=0.0f; x<1.0f; x += 0.00001f);
@@ -12,11 +21,9 @@ void delay() {
 int main()
 {
 
-    init_leds();
-
-    /* Button initialization */
-    RCC->AHBENR |= (1 << 17);     /* enable GPIO A clock */
-    GPIOA->MODER |= 0x00000000;   /* configure A0 for digital input */
+    stm32f3_init_leds();
+    stm32f3_init_button();
+    
 
     while(1) {
         if (!(GPIOA->IDR & 0x00000001))
@@ -25,14 +32,14 @@ int main()
     }
 }
 
-void init_leds(void) 
+void stm32f3_init_leds(void) // allume le peripherique et initialise les pins
 {
     /* LEDs initialization */
     RCC->AHBENR |= (1 << 21);     /* enable GPIO E clock */
     GPIOE->MODER |= 0x55550000;   /* configure E8-E15 for output */
 }
 
-void set_leds(char num, char v)
+void stm32f3_set_leds(char num, char v)// num=numero de la LED (0-7), v= 1 (allume) ou 0 (eteint)
 {
     if(v){
         GPIOE->ODR |= (v << num + 8); 
@@ -41,11 +48,43 @@ void set_leds(char num, char v)
     }
 }
 
-char get_leds(char num)
+char stm32f3_get_leds(char num)
 {
    return GPIOE->IDR & (1 << num + 8) ? 1 : 0;  
 }
 
+void init_button(){ // allume le peripherique et initialise les pins
+    /* Button initialization */
+    RCC->AHBENR |= (1 << 17);     /* enable GPIO A clock */
+    GPIOA->MODER |= 0x00000000;   /* configure A0 for digital input */
+    
+    button_state = stm32f3_get_button();
+}
+
+char stm32f3_get_button(){ // renvoie 1 si bouton appuye, 0 sinon
+    return GPIOA->IDR & (1 << 17) ? 1 : 0; 
+}
+
+char stm32f3_just_pressed(){  // renvoie TRUE si le bouton a change d’etat (de 0 vers 1) depuis le dernier appel a cette fonction
+    
+    if(!button_state && stm32f3_get_button()){
+        button_state = !button_state;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+char stm32f3_just_released(){
+    if(button_state && !stm32f3_get_button()){
+        button_state = !button_state;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
 
