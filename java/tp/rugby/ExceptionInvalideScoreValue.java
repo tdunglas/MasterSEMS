2e020e9b3ebae1c01b3b16be528e678b91705281
licencep <licencep@infotp16.cnam.fr>
2018-04-09 10:05:28 +0200

    remove folder from main branch

--- java/tp/rugby/ExceptionInvalideScoreValue.java
+++
@@ -1,6 +0,0 @@
-public class ExceptionInvalideScoreValue extends Exception{
-    
-    public ExceptionInvalideScoreValue(int val){
-        System.out.println("Invalide score value for rugby match, " + val);
-    }
-}
--- java/tp/rugby/ExceptionMatchFinished.java
+++
@@ -1,6 +0,0 @@
-public class ExceptionMatchFinished extends Exception{
-    
-    public ExceptionMatchFinished(){
-        System.out.println("Rugby match already finished can't modify score");
-    }
-}
